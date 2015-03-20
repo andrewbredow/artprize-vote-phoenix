99825ec664b43b322e3bb6a2b38148b6270f3f0a
Andrew Bredow <bredow@gmail.com>
2015-03-20 02:01:32 -0400

    Remove protect from forgery for easy demoing

--- web/router.ex
+++ web/router.ex
@@ -5,7 +5,7 @@ defmodule Artprize.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
+    # plug :protect_from_forgery
   end
 
   pipeline :api do
